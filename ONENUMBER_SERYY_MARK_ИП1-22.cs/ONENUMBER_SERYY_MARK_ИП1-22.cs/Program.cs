80bb3bcd9aef7f4ad1d5392f87b773136bcbd193
SOUL18998 <146361753+SOUL18998@users.noreply.github.com>
2024-12-04 12:43:30 +0300

    Add files via upload

---
+++ ONENUMBER_SERYY_MARK_ИП1-22.cs/ONENUMBER_SERYY_MARK_ИП1-22.cs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace edinoe_chislo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество чисел:");
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];  
+
+           
+            Console.WriteLine("Введите числа по одному:");
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());  
+            }
+
+            
+            bool foundUnique = false;
+
+            
+            Console.WriteLine("Уникальные числа, которые появляются только один раз:");
+
+            // Ищем уникальные числа
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool isUnique = true;
+
+                // Считаем, сколько раз встречается текущее число
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j && nums[i] == nums[j])
+                    {
+                        // Если найдено совпадение, число не уникальное
+                        isUnique = false;
+                        break;
+                    }
+                }
+
+                // Если число уникальное, выводим его
+                if (isUnique)
+                {
+                    Console.WriteLine(nums[i]);
+                    foundUnique = true;
+                }
+            }
+
+            // Если уникальные числа не были найдены
+            if (!foundUnique)
+            {
+                Console.WriteLine("Числа, которые появляются только один раз, не найдены.");
+            }
+        }
+    }
+}
